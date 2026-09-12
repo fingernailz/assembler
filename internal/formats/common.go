fe2b41856fec54d0fae65671b1f623e4af6bfd51
fingernailz <sanjay.pandian@tutamail.com>
2026-09-12 23:59:49 +0530

    differnt formats and format creation

--- internal/formats/common.go
+++ internal/formats/common.go
@@ -1,7 +1,52 @@
 package formats
 
-import "github.com/fingernailz/assembler/internal/models"
+import (
+	"strings"
 
-func CreateInstruction(instruction string) (*models.Instruction, error) {
-	return nil, nil
+	"github.com/fingernailz/assembler/internal/isa"
+	"github.com/fingernailz/assembler/internal/models"
+)
+
+const (
+	emptyString string = ""
+	space       string = " "
+)
+
+// rename this type later
+type NewTyep struct {
+	InstructionSet []*models.Instruction
+	SymbolTablePtr *map[string]int
+}
+
+func CreateFormats(instructions []string, stptr *map[string]int) *NewTyep {
+
+	for _, instr := range instructions {
+
+		// doing this again in second pass
+		instr = strings.TrimSpace(instr)
+
+		if instr == emptyString {
+			continue
+		}
+
+		instrSlice := strings.Split(instr, " ")
+
+		mappedValue, ok := isa.Instructions[strings.ToUpper(instrSlice[0])]
+
+		if !ok {
+			//errors
+			// return nil, new errors("error")
+		}
+
+		switch mappedValue["format"] {
+		case isa.R_FORMAT:
+		case isa.I_FORMAT:
+		case isa.D_FORMAT:
+		case isa.B_FORMAT:
+		case isa.CB_FORMAT:
+		case isa.IW_FORMAT:
+		}
+	}
+
+	return nil
 }
